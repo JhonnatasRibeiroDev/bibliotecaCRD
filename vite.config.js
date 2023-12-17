df9850f3d47d9ac55ccd6feac19f2983ac6007f1
Jhonnatas Ribeiro <jhonnatasribeiro01@gmail.com>
2023-12-17 09:10:45 -0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import ViteReact from '@vitejs/plugin-react';
+import { createProxy } from 'vite-plugin-mock';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [ViteReact(), createProxy('/api', { target: 'https://serverapi.jhonnatasribeir.repl.co' })],
   base: "/bibliotecaCRD",
 
   
